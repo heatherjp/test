b4bc54ed77f74b3ff5d42f444167d8761149e78c
Peng Jiang <PJiang@pembina.com>
2020-06-22 17:55:43 -0600

    3

--- test/__init__.py
+++ test/__init__.py
@@ -1,4 +1,11 @@
 import text
-
+#from text import hello_from_text
 def hello():
     return ("Hello world")
+
+
+if __name__ == "__main__":
+    print(hello())
+    print(text.hello_from_text()) 
+else:
+    print(text.hello_from_text()) 
