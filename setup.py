a34d2beab6afe94df6ed9f10675a0ec3013bacc5
Peng Jiang <PJiang@pembina.com>
2020-06-22 15:34:27 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='test',
+      version='0.1',
+      description='pip install test repo',
+      url='https://github.com/heatherjp/test',
+      author='Peng Jiang',
+      author_email='pjiang@pembina.com',
+      license='MIT',
+      packages=['test'],
+      zip_safe=False)
