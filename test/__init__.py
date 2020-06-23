e03d92b2eab68daa09696c7d973d1b4dc4c3820e
Peng Jiang <PJiang@pembina.com>
2020-06-22 18:17:38 -0600

    delete main

--- test/__init__.py
+++ test/__init__.py
@@ -2,10 +2,3 @@ import text
 #from text import hello_from_text
 def hello():
     return ("Hello world")
-
-
-if __name__ == "__main__":
-    print(hello())
-    print(text.hello_from_text()) 
-else:
-    print(text.hello_from_text()) 
