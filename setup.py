77fa38ff55d5f4ac4b4b8ec14b362af9cba881bb
Peng Jiang <PJiang@pembina.com>
2020-06-22 21:39:40 -0600

    rename

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='test',
+setup(name='test_package',
       version='0.1',
       description='pip install test repo',
       url='http://github.com/heatherjp/test',
       author='Peng Jiang',
       author_email='pjiang@pembina.com',
       license='MIT',
-      packages=['test'],
+      packages=['test_package'],
       zip_safe=False)
