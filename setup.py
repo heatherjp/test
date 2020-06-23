5e735c4413e2d68c74736f8449ca88f4ba466e3f
Peng Jiang <PJiang@pembina.com>
2020-06-22 21:30:27 -0600

    python setup.py sdist

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='test',
       version='0.1',
       description='pip install test repo',
-      url='https://github.com/heatherjp/test',
+      url='http://github.com/heatherjp/test',
       author='Peng Jiang',
       author_email='pjiang@pembina.com',
       license='MIT',
